8f9a96d74021c5187eb0010e08670ba0e06aed66
MD63MId <manju.sahu@ing.com>
2019-09-09 21:09:58 +0200

    Initial commit

---
+++ src/main/java/com/fsd/fsdresttaskmanager/FsdRestTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fsd.fsdresttaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdRestTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdRestTaskManagerApplication.class, args);
+	}
+
+}
