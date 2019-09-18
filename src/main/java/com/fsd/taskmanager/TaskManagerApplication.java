73af2e49d35495610e26fe1d21884570b0e6a4ec
MD63MId <manju.sahu@ing.com>
2019-09-18 16:05:21 +0200

    War packaging

--- src/main/java/com/fsd/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/fsd/taskmanager/TaskManagerApplication.java
@@ -2,16 +2,17 @@ package com.fsd.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class TaskManagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
@@ -33,6 +34,11 @@ public class TaskManagerApplication {
                         .maxAge(-1)  ;
             }
         };
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TaskManagerApplication.class);
     }
 
 
